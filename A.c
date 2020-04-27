e4d18b340520e4d0f93d5c8614518416de5c591f
benoky <czmn@naver.com>
2020-04-27 13:10:33 +0900

    test

---
+++ A.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+sdasd
+asd
+asda
+sdas
+dasd
+
+void func1();
+void func2();
+int main()
+{
+	printf("hello");
+
+	func1();
+	func2();
+
+	return();
+}
+
+void func1(){
+	prinf("func1");
+}
+void func2(){
+	printf("asdasdasd");
+}
+
